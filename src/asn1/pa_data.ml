5a979293e0f2e6f0b679c5a263d425ec07af1ee9
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2015-06-06 20:22:15 -0400

    padata

--- src/asn1/pa_data.ml
+++ src/asn1/pa_data.ml
@@ -1,3 +1,19 @@
-(* unimplemented*)
+open Import;;
 
-include Dummy_asn1
+type t =
+  { padata_type : Krb_int32.t
+  ; padata_value : string
+  }
+
+module Format = struct
+  type t = Krb_int32.Format.t * Cstruct.t
+
+  let asn =
+    sequence2
+      (* First value is 1 not 0 *)
+      (tag_required ~label:"padata_type" 1 Krb_int32.Format.asn)
+      (tag_required ~label:"padata_value" 2 octet_string)
+end
+
+let format_of_t t =
+  (Krb_int32.format_of_t t.padata_type, Cstruct.of_string t.padata_value)
