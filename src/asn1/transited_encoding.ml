a3b45f51b03f0e26b3e7447d5f255f711e337f97
Brandon Bohrer <bbohrer@andrew.cmu.edu>
2015-07-13 21:18:04 -0400

    transited encoding

--- src/asn1/transited_encoding.ml
+++ src/asn1/transited_encoding.ml
@@ -1,1 +1,19 @@
-include Dummy_asn1
+open Import;;
+
+type t =
+  { tr_type : Krb_int32.t
+  ; contents : Octet_string.t
+  }
+
+module Format = struct
+  type t = Krb_int32.Format.t * Octet_string.Format.t
+
+  let asn =
+    sequence2
+      (tag_required ~label:"tr_type" 0 Krb_int32.Format.asn)
+      (tag_required ~label:"contents" 1 Octet_string.Format.asn)
+end
+
+let format_of_t t =
+  ( Krb_int32.format_of_t t.tr_type
+  , Octet_string.format_of_t t.contents)
